d417a39949bf4d77f1ef4deb968439c15d51221a
Felix <felixmooij2@gmail.com>
2019-04-16 11:19:53 +0200

    first working implementation dijkstra

---
+++ connection.py
@@ -0,0 +1,14 @@
+class Connection(object):
+    """
+    Representation of a connection in RAILNL
+    """
+
+    def __init__(self, name, id, station1, station2, time):
+        """
+        Initializes a Connection
+        """
+        self.name = name
+        self.id = id
+        self.stat1 = station1
+        self.stat2 = station2
+        self.time = time
---
+++ train.py
@@ -0,0 +1,9 @@
+
+# Implements a train object
+class Train:
+
+    def __init__(self, startingPoint, endingPoint):
+
+        self.stationsVisited = []
+        self.startingPoint = startingPoint
+        self.endingPoint = endingPoint
