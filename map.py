e6ed33a9702389631135e3f2a8736514ea266035
Bram Schmidt <bram_schmidt@hotmail.com>
2019-04-17 10:50:29 +0200

    map

--- map.py
+++ map.py
@@ -14,14 +14,22 @@ if __name__ == "__main__":
                   urcrnrlon=5.4, urcrnrlat=53.2)
 
     load_stations(INPUT_STATIONS)
+    load_connections(INPUT_CONNECTIONS)
 
     for station in stations:
         lon = float(stations[station].yCoordinate)
         lat = float(stations[station].xCoordinate)
         x,y = map(lon, lat)
         label = stations[station].name
-        plt.text(x, y +5000, label, fontsize = 6)
-        map.plot(x, y, 'ko', markersize=6)
+        #plt.text(x, y +5000, label, fontsize = 6)
+        #map.plot(x, y, 'ko', markersize=6)
+
+    for connection in connections:
+        lon1 = float(stations[str(connections[connection].stat1)].yCoordinate)
+        lat1 = float(stations[str(connections[connection].stat1)].xCoordinate)
+        lon2 = float(stations[str(connections[connection].stat2)].yCoordinate)
+        lat2 = float(stations[str(connections[connection].stat2)].xCoordinate)
+        plt.plot([lat1,lat2], [lon1,lon2],marker='o', linewidth=5)
 
     map.drawcoastlines()
     map.drawcountries()
