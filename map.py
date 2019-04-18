08b9672255f9ff55a7423389f77908052ce9ab0a
Bram Schmidt <bram_schmidt@hotmail.com>
2019-04-18 14:05:19 +0200

    map update

--- map.py
+++ map.py
@@ -4,36 +4,49 @@ import numpy as np
 from mainActivity import Station, Connection, connections, stations, load_stations, load_connections, INPUT_STATIONS, INPUT_CONNECTIONS
 from mpl_toolkits.basemap import Basemap
 
-INPUT_CSV = "StationsHolland.csv"
+INPUT_STATIONS = "StationsHolland.csv"
+INPUT_CONNECTIONS = "ConnectiesHolland.csv"
 
-if __name__ == "__main__":
+def load_map():
 
     map = Basemap(projection='merc', lat_0 = 52, lon_0 = 5,
                   resolution = 'h', area_thresh = 0.1,
                   llcrnrlon=4, llcrnrlat=51.7,
                   urcrnrlon=5.4, urcrnrlat=53.2)
+    map.drawcoastlines()
+    map.drawcountries()
+    map.fillcontinents(color = 'coral')
+    map.drawmapboundary()
 
-    load_stations(INPUT_STATIONS)
-    load_connections(INPUT_CONNECTIONS)
+def draw_stations(stations):
 
     for station in stations:
         lon = float(stations[station].yCoordinate)
         lat = float(stations[station].xCoordinate)
         x,y = map(lon, lat)
         label = stations[station].name
-        #plt.text(x, y +5000, label, fontsize = 6)
-        #map.plot(x, y, 'ko', markersize=6)
+        plt.text(x, y +5000, label, fontsize = 6)
+        map.plot(x, y, 'ko', markersize=6)
+
+def draw_connections(stations, connections):
 
     for connection in connections:
-        lon1 = float(stations[str(connections[connection].stat1)].yCoordinate)
-        lat1 = float(stations[str(connections[connection].stat1)].xCoordinate)
-        lon2 = float(stations[str(connections[connection].stat2)].yCoordinate)
-        lat2 = float(stations[str(connections[connection].stat2)].xCoordinate)
-        plt.plot([lat1,lat2], [lon1,lon2],marker='o', linewidth=5)
+        lon = []
+        lat = []
+        lon.append(float(stations[str(connections[connection].stat1)].yCoordinate))
+        lat.append(float(stations[str(connections[connection].stat1)].xCoordinate))
+        lon.append(float(stations[str(connections[connection].stat2)].yCoordinate))
+        lat.append(float(stations[str(connections[connection].stat2)].xCoordinate))
+        x,y = map(lon, lat)
+        map.plot(x, y, 'k-', markersize=5, linewidth=1)
 
-    map.drawcoastlines()
-    map.drawcountries()
-    map.fillcontinents(color = 'coral')
-    map.drawmapboundary()
+if __name__ == "__main__":
+
+    load_map()
+    load_stations(INPUT_STATIONS)
+    load_connections(INPUT_CONNECTIONS)
+
+    draw_stations(stations)
+    draw_connections(stations, connections)
 
     plt.show()
