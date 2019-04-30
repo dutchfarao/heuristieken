6e0e90bfc0cd74a8103bec199958db4f1d4f6834
Felix <felixmooij2@gmail.com>
2019-04-30 14:26:04 +0200

    Delete all the files

--- hillClimber.py
+++
@@ -1,1 +0,0 @@
-from mainActivity import Station, Connection, connections, stations, load_stations, load_connections, INPUT_STATIONS, INPUT_CONNECTIONS
--- train.py
+++
@@ -1,9 +0,0 @@
-
-# Implements a train object
-class Train:
-
-    def __init__(self, startingPoint, endingPoint):
-
-        self.stationsVisited = []
-        self.startingPoint = startingPoint
-        self.endingPoint = endingPoint
