a9e7495cd057387c90a35c777f3ce4105d80951d
Bram Schmidt <bram_schmidt@hotmail.com>
2019-04-30 18:21:00 +0200

    dinges

--- bram_test.py
+++ bram_test.py
@@ -9,5 +9,5 @@ INPUT_STATIONS = "Data/StationsHolland.csv"
 if __name__ == "__main__":
     load_stations(INPUT_STATIONS)
     load_connections(INPUT_CONNECTIONS)
-    print(stations["Gouda"].destinations)
-    print(connections)
+    station = input("Station: ").title()
+    print("Verbindingen: ", stations[station].adjacent)
