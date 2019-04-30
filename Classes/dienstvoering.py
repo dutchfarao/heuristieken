a70f3566b461611031db7369175e34c12510535d
Darian <darianelsayed@hotmail.com>
2019-04-30 16:59:37 +0200

    completed Dienstvoering class

--- Classes/dienstvoering.py
+++ Classes/dienstvoering.py
@@ -1,3 +1,4 @@
+from score import Score
 # Implements a Dienstvoering object
 class Dienstvoering:
     """
@@ -6,10 +7,19 @@ class Dienstvoering:
 
     def __init__(self, dienstId):
         """
-        Initializes a Station
+        Initializes a Dienstvoering
         """
         self.dienstId = dienstId
+        self.score = 0
         self.trajects = {}
-        self.connections = []
-        self.critical_connections = []
-        self.connections = []
+        self.connections_visited = []
+        self.critical_visited = []
+
+def fill_connections(self, connection):
+    self.connections_visited.append(connection)
+
+def fill_critical(self, critical):
+    self.critical_visited.append(critical)
+
+def set_score(self, p, m, t):
+        self.score = (10000 * self.p) - (self.t * 20 + self.m / 10)
