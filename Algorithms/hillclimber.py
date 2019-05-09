03bc7a2544a0777be925076398f15fe445d16f37
Bram Schmidt <bram_schmidt@hotmail.com>
2019-05-09 15:40:11 +0200

    hillclimber update

--- Algorithms/hillclimber.py
+++ Algorithms/hillclimber.py
@@ -6,9 +6,17 @@ from main import *
 def Hillclimber():
 
     hillclimber_dienstvoeringen = {}
+    lowest_score = 10000
 
-    for i in range(10):
-        d = Random()
+    for i in range(3):
+
+        for i in range(10):
+            d = Random()
+            if d.score < lowest_score:
+            lowest_score = d.score
+
+        if hillclimber_dienstvoeringen.value.score == lowest_score
+            remove
         hillclimber_dienstvoeringen[i] = d
 
-    print(hillclimber_dienstvoeringen[0].value.score)
+    print(hillclimber_dienstvoeringen.values.dienstId)
