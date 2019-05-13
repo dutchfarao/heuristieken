d8b1c5f8570b1e46e93be712b8bc151984d5b622
Felix <felixmooij2@gmail.com>
2019-05-13 15:00:58 +0200

    More work on visualisations + matplotlib

--- HelperFunctions/VisualisationHelper.py
+++ HelperFunctions/VisualisationHelper.py
@@ -1,6 +1,17 @@
 import csv
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 import numpy as np
+plt.style.use('fivethirtyeight')
+plt.rcParams.update({'figure.autolayout': True})
+
+def currency(x, pos):
+    """The two args are the value and tick position"""
+    if x >= 1e6:
+        s = '{:1.1f}M'.format(x*1e-6)
+    else:
+        s = '{:1.0f}000'.format(x*1e-3)
+    return s
 
 # Prints out the average, lowest and highest score for on scores_dict
 def CategoricalPlot(scores):
@@ -25,5 +36,62 @@ def CategoricalPlot(scores):
     plt.suptitle('Categorical Plotting')
     plt.show()
 
-def GraphPlot(scores):
+# This function calculates the lowest, highest and average K of the results of each algorithm
+def Calculator(scores_greedy, scores_random):
+
+    n_greedy = len(scores_greedy)
+    n_random = len(scores_random)
+
+    total_greedy = sum(scores_greedy.values())
+    total_random = sum(scores_random.values())
+
+    average_greedy = total_greedy / n_greedy
+    average_random = total_random / n_random
+    #print(average_greedy)
+    #print(average_random)
+
+    high_greedy = max(scores_greedy.values())
+    low_greedy = min(scores_greedy.values())
+
+    high_random = max(scores_random.values())
+    low_random = min(scores_random.values())
+
+    data = {"Greedy" : average_greedy, "Random" : average_random, "Hillclimber" : 3235, "Hillclimber SA" : 3604, "Random + Dijkstra" : 2834}
+    group_data = list(data.values())
+    group_names = list(data.keys())
+    group_mean = np.mean(group_data)
+
+    formatter = FuncFormatter(currency)
+
+    fig, ax = plt.subplots()
+    ax.barh(group_names, group_data)
+    labels = ax.get_xticklabels()
+    plt.setp(labels, rotation=45, horizontalalignment='right')
+    ax.set(xlim=[0, 6000], xlabel='Average K', ylabel='Algorithm', title='Difference in averages')
+    ax.xaxis.set_major_formatter(formatter)
+    ax.axvline(average_random, ls='--', color='r')
+    ax.title.set(y=1.05)
+
+    # Annotate new companies
+    for group in [0]:
+        ax.text(5000, group, "-1%", fontsize=10,
+                verticalalignment="center")
+
+    for group in [2]:
+        ax.text(5000, group, "+3%", fontsize=10,
+                verticalalignment="center")
+
+    for group in [3]:
+        ax.text(5000, group, "+12%", fontsize=10,
+                verticalalignment="center")
+
+    for group in [4]:
+        ax.text(5000, group, "-6%", fontsize=10,
+                verticalalignment="center")
+
+    plt.show()
+
+    # Uncomment this line to save the figure.
+    # fig.savefig('sales.png', transparent=False, dpi=80, bbox_inches="tight")
+
     pass
--- main.py
+++ main.py
@@ -6,13 +6,29 @@ from Algorithms.greedy import *
 from Algorithms.random import *
 import time
 from HelperFunctions.VisualisationHelper import *
-
-#from Algorithms.hillclimber import *
+from Algorithms.hillclimber import *
 
 if __name__ == "__main__":
 
-
     scores_dict = {}
+    data = {}
+
+    choiceAction = input("Would you like to run algorithms or perform visualisations? Select 'a' or 'v'")
+    if choiceAction == 'v':
+
+            scores_dict_greedy = ReadScores("Greedy")
+            scores_dict_random = ReadScores("Random")
+            #scores_dict_hillclimber =
+            #scores_dict_hillclimberSA =
+            Calculator(scores_dict_greedy, scores_dict_random)
+            choiceVisualisation = input("What kind of visualisation would you like? ")
+            scores_dict = ReadScores(choiceAlgorithm)
+            if choiceVisualisation == "CatPlot":
+                CategoricalPlot(scores_dict)
+
+            elif choiceVisualisation == "Graph":
+                GraphPlot(scores_dict)
+
     choiceRegion = input("Dienstregeling voor heel Nederland of Noord en Zuid Holland? ")
 
     if choiceRegion == "Nederland":
@@ -28,7 +44,7 @@ if __name__ == "__main__":
     #print(g.station_dict)
     #print(g.station_dict["Alkmaar"].adjacent)
 
-    print("Inputs are: Random, Greedy, Hillclimber or Depth-first.")
+    print("Inputs are: Random, Greedy, Hillclimber or HillclimberSA.")
     choiceAlgorithm = input("Please specify which algorithm you want to use: ")
 
     if choiceAlgorithm == "Greedy":
@@ -63,12 +79,6 @@ if __name__ == "__main__":
         ReadScores(choiceAlgorithm)
 
     elif choiceAlgorithm == "Hillclimber":
-        Hillclimber()
-
-    choiceVisualisation = input("What kind of visualisation would you like? ")
-    scores_dict = ReadScores(choiceAlgorithm)
-    if choiceVisualisation == "CatPlot":
-        CategoricalPlot(scores_dict)
-
-    elif choiceVisualisation == "Graph":
-        GraphPlot(scores_dict)
+        hillclimber_size = int(input("Please specify the number of times (integer) you want to run Hillclimber: "))
+        d, P, P_traject, MIN, MIN_traject = Random(1)
+        Hillclimber(d, hillclimber_size, P, P_traject, MIN, MIN_traject)
